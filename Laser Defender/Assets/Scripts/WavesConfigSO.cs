7a2e2ba21295f4c8401b0f12520221008c89056f
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2023-12-31 16:54:36 +0700

    update empty repo

---
+++ Laser Defender/Assets/Scripts/WavesConfigSO.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = " New Wave Config", menuName = "Wave Config")]
+public class WavesConfigSO : ScriptableObject 
+{
+    [SerializeField] List<GameObject> enemyPrefabs;
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance = 0f;
+    [SerializeField] float minimumSpawnTime = 0.2f;
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
+
+    public Transform GetStartingWayPoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> wayPoints = new List<Transform>();
+        foreach (Transform child in pathPrefab)
+        {
+            wayPoints.Add(child);
+        }
+        return wayPoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance,
+                                        timeBetweenEnemySpawns + spawnTimeVariance);
+        return Mathf.Clamp(spawnTime, minimumSpawnTime, float.MaxValue);
+    }
+  
+}
